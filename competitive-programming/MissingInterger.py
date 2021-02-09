aa15a2505498cf3378e839e448a17724733cd68f
Bhavana <mbbhavana9@gmail.com>
2021-02-09 15:10:13 -0600

    correct code

--- competitive-programming/MissingInterger.py
+++ competitive-programming/MissingInterger.py
@@ -1,13 +1,16 @@
-def missing_integer(A):
-    A = sorted(A)
-    min = 1
-    for i in range(1, max(A)):
-        if i == min:
-            i += 1
-        else:
-            return i
+
+def solution(A):
+    # write your code in Python 3.6
+    pos = [False]*(len(A)+1)
+    for i in A:
+        if i in range(1, len(A)+1):
+            pos[i-1] = True
+    for i in range(len(pos)+1):
+        if pos[i] == False:
+            return i+1
+    return 1
 
 
-miss_val = missing_integer([1, 4, 5, 3])
+miss_val = solution([1, 4, 5, 10, 11, 9, 23, 34, 2, 6, 7, 7, 3])
 
 print(miss_val)
