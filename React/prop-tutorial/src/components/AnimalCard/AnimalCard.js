41ee1f51fc7a3fcf3420a4e1e3cce2d8c709ed1c
Bhavana <mbbhavana9@gmail.com>
2021-02-21 20:12:26 -0600

    prop changes

--- React/prop-tutorial/src/components/AnimalCard/AnimalCard.js
+++ React/prop-tutorial/src/components/AnimalCard/AnimalCard.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './AnimalCard.css';
+import PropTypes from 'prop-types'; 
 
-export default function AnimalCard({additional,diet,name,scientificName,size}){
+export default function AnimalCard({
+  additional,
+  diet,
+  name,
+  scientificName,
+  showAdditional,
+  size
+}) {
+  return (
+    <div className="animal-wrapper">
+      <h2>{name}</h2>
+      <h3>{scientificName}</h3>
+      <h4>{size}kg</h4>
+      <div>{diet.join(', ')}.</div>
+      <button onClick={() => showAdditional(additional)}>More Info</button>
+    </div>
+  );
+}
 
-// const {name} = props
-return (<div><h2>{name}</h2>
-     <h3>{scientificName}</h3>
-     <h4>{size}kg</h4>
-     <div>{diet.join(', ')}.</div>
-      </div> 
-);
+AnimalCard.protoTypes ={
+additional: PropTypes.shape({
+link:PropTypes.string,
+notes: PropTypes.string
+}),
+diet: PropTypes.oneOfType([PropTypes.number,PropTypes.string]).isRequired,
+name: PropTypes.string.isRequired,
+scientificName:PropTypes.string.isRequired,
+showAdditional:PropTypes.func.isRequired,
+size:PropTypes.number.isRequired,
+}
+AnimalCard.defaultProps ={
+additional:{
+notes:'No Additional Information'
+}
 }
--- React/prop-tutorial/src/components/App/App.js
+++ React/prop-tutorial/src/components/App/App.js
@@ -4,21 +4,33 @@ import data from './data';
 import AnimalCard from '../AnimalCard/AnimalCard';
 import './App.css'
 
+//showAdditional converts an object to array of pairs ( first element is key and second is value)
+//maps data converting key-value to string
+
+function showAdditional(additional) {
+  const alertInformation = Object.entries(additional)
+    .map(information => `${information[0]}: ${information[1]}`)
+    .join('\n');
+  alert(alertInformation)
+};
+
 function App() {
   return (
     <div className="wrapper">
-    <h1>Animals</h1>
-    {data.map(animal =>(
-      <AnimalCard 
-      addtional = {animal.additional}
-      diet = {animal.diet}
-      key={animal.name}
-      name={animal.name}
-      scientificName = {animal.scientificName}
-      size = {animal.size}/>
-    ))}
-</div>
-);
+      <h1>Animals</h1>
+      {data.map(animal => (
+        <AnimalCard
+          additional={animal.additional}
+          diet={animal.diet}
+          key={animal.name}
+          name={animal.name}
+          scientificName={animal.scientificName}
+          showAdditional={showAdditional}
+          size={animal.size}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App;
